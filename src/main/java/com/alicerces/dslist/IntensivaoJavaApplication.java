6d217cd251926cf03bfc33849743267c4e1b9a4b
Rinaldo Belisario Junior <rinaldobjr@gmail.com>
2023-07-31 01:09:16 -0400

    Criando Projeto

---
+++ src/main/java/com/alicerces/dslist/IntensivaoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.alicerces.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivaoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivaoJavaApplication.class, args);
+	}
+
+}
